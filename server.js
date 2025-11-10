5cc18eb8e70525d2131123eae15cbb8f15282a82
D3bbie100 <113763116+D3bbie100@users.noreply.github.com>
2025-11-10 15:47:02 +0300

    Removed public folder

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(express.static("."));
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public/index.html"));
